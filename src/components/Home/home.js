0392a1820bbdc43f904d63580c2a8540438848ab
Nisal <nisatt11@gmail.com>
2024-01-03 20:53:52 -0500

    Finish about description and styling

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,14 +6,19 @@ const Home = () => {
     return (
         <section id='intro' >
             <div className="intro-text">
-                <span className="name">Nisal Attanayake</span>
-                <span className=""></span>
+                <span className="description-1">SOFTWARE ENGINEER +</span>
+                <br></br>
+                <span className="description-2">FULL-STACK DEVELOPER</span>
+                <br></br>
+                <span className="name">NISAL ATTANAYAKE</span>
+                <p className="statement">I'm a dynamic and passionate full-stack software engineer, who brings a fresh perspective to the technology industry. 
+                My proficiency lies in my exceptional ability to debug complex code, research and integrate cutting-edge technologies, and tackle challenging projects with tenacity and a solution-driven approach. 
+                I am committed to continuously learning and poised to make an impact by delivering innovative solutions.</p>
             </div>
 
             <div className="skillWheel">
-
+            
             </div>
-
         </section>
     )
 }
