26d7e6e5aa95b1e2167af7ceca27a55e49d11d72
Nisal <nisatt11@gmail.com>
2024-01-04 00:02:03 -0500

    Add projects and style

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,10 +1,14 @@
 import './projects.css';
+import boomerbot from '../../assets/boomerbot.png';
+import togethr from '../../assets/togethr.png';
+import flappy from '../../assets/flappy.png';
+
 
 const projectsData = [
     {
         title: 'Flappy Bird Clone',
         description: 'My first Javasript project. Created a clone of the popular mobile game Flappy Bird using Javascript, canvas, and DOM manipulation.',
-        image: 'img',
+        image: flappy,
         githubLink: 'https://github.com/Nisal4/Project_1?tab=readme-ov-file',
         link: 'https://nisal4.github.io/Project_1/', 
     },
@@ -12,7 +16,7 @@ const projectsData = [
     {
         title: 'Togethr App',
         description: 'My first group project with collaborators Adriana Saavedra and Kevin Tran. This app was made using Python, Django, and SQL. It is a twitter clone that allows users to create a profile, posts, comments, and follow other users.',
-        image: 'img',
+        image: togethr,
         githubLink: 'https://github.com/Nisal4/togethr-app',
         link: 'https://togethr-31dc444c8abd.herokuapp.com/', 
     },
@@ -20,7 +24,7 @@ const projectsData = [
     {
         title: 'BoomerBot',
         description: 'My second group project with collaborators Adriana Saavedra, Ahmad Wali, and Krystalin Castillo. This app was made using React and Express. It is a chatbot that talks to the user as if they were a boomer by implementing googles Gemini Pro Ai API.',
-        image: 'img',
+        image: boomerbot,
         githubLink: 'https://github.com/Nisal4/BoomerBot/tree/main',
         link: 'https://boomerbot-c3175a34bfb5.herokuapp.com/', 
     },
@@ -30,15 +34,17 @@ const Projects = () => {
     return (
         <div className='projects-container'>
             <div className='content'>
-                <span className='header'>Projects</span>
+                <span className='header'>PROJECTS</span>
                 <div className='project-list'>
                     {projectsData.map((project, index) => (
                         <div className='project' key={index}>
                             <div className='project-title'>{project.title}</div>
-                            <div className='project-img'>{project.img}</div>
+                            <img src={project.image} className='project-img' alt='project-img' />
                             <div className='project-description'>{project.description}</div>
-                            <a className='github-link' href={project.githubLink}>Github Link</a>
-                            <a className='project-link' href={project.link}>View Project</a>
+                            <div className='link-container'>
+                                <a className='github-link' href={project.githubLink} target="_blank" rel="noopener noreferrer">Github Link</a>
+                                <a className='project-link' href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
+                            </div>
                         </div>
                     ))}
                 </div>
